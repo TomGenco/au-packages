bf1393f57a0bd69f8d221ef3ecde3325281901f1
Tom Genco <tom@tomgenco.com>
2024-02-07 22:43:19 -0700

    Rewrote au_GetLatest and cleaned up chocolateyinstall.ps1

--- protonmail/tools/chocolateyinstall.ps1
+++ protonmail/tools/chocolateyinstall.ps1
@@ -1,22 +1,23 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url        = 'https://github.com/ProtonMail/inbox-desktop/releases/download/v0.9.3/Proton.Mail.Beta-0.9.3.Setup.exe'
+$url      = 'https://github.com/ProtonMail/inbox-desktop/releases/download/v0.9.3/Proton.Mail.Beta-0.9.3.Setup.exe'
+$checksum = '3794278bf13f713c229ffb352ffbc3a5b743481787c4dfc66076f9d54d4e3b47'
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'exe'
-  url           = $url
+  packageName    = $env:ChocolateyPackageName
+  unzipLocation  = $toolsDir
+  fileType       = 'exe'
+  url            = $url
 
-  softwareName  = 'Proton Mail Beta*'
+  softwareName   = 'Proton Mail Beta*'
 
-  checksum      = '3794278BF13F713C229FFB352FFBC3A5B743481787C4DFC66076F9D54D4E3B47'
-  checksumType  = 'sha256'
+  checksum       = $checksum
+  checksumType   = 'sha256'
 
-  silentArgs    = '-s'
-  validExitCodes= @(0)
+  silentArgs     = '-s'
+  validExitCodes = @(0)
 }
 
 Install-ChocolateyPackage @packageArgs
--- protonmail/update.ps1
+++ protonmail/update.ps1
@@ -1,21 +1,27 @@
 import-module au
 
-$releases = 'https://github.com/ProtonMail/inbox-desktop/releases'
+$releases = 'https://api.github.com/repos/ProtonMail/inbox-desktop/releases/latest'
 
 function global:au_SearchReplace {
     @{
         "tools\chocolateyInstall.ps1" = @{
-            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"           #1
-            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"      #2
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
     }
 }
 
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -UseBasicParsing
-    $regex   = '.exe$'
-    $url     = $download_page.links | ? href -match $regex | select -First 1 -expand href
-    $version = $url -split '-|.Setup.exe' | select -Last 1 -Skip 2
+
+    $url_regex = '[^"]+/Proton.Mail.Beta-[^"]+.Setup.exe'
+    $url = $download_page.content -match $url_regex
+    $url = $matches[0]
+
+    $version_regex = '/releases/download/v([^/]+)/'
+    $version = $url -match $version_regex
+    $version = $matches[1]
+
     return @{ Version = $version; URL32 = $url }
 }
 
