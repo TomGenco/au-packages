b06f1924696e0ce4851848fbe67aa3b7e1423b79
Chocolatey <chocolatey@realdimensions.net>
2024-06-23 06:25:10 +0000

    AU: 1 updated - protonmail [skip ci] https://gist.github.com/TomGenco/2e23d78705cb8907654c5d596f75ebfc/b0867c71320544aaeeca668fcedcc644a039cdce

--- protonmail/tools/chocolateyinstall.ps1
+++ protonmail/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-$url      = 'https://github.com/ProtonMail/inbox-desktop/releases/download/v1.0.2/Proton-Mail-1.0.1-Setup.exe'
-$checksum = '19fee55e7a6235699c297e16fb04fcebdbe9b4c075d548d19e919706e92dc182'
+$url      = 'https://github.com/ProtonMail/inbox-desktop/releases/download/v1.0.3/Proton.Mail-1.0.3.Setup.exe'
+$checksum = '9a43d27ae38c4d765f5adacce5e5e10a0cc1615a3c7e5d34121a0ad11459cafc'
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
